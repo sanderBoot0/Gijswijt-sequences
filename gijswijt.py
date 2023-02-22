1327a7878e43cdb7f74f951545b2e8f648573902
= <sander.boot.0@outlook.com>
2023-02-22 17:03:12 +0100

    cleaned up code

--- gijswijt.py
+++ gijswijt.py
@@ -12,17 +12,13 @@ hasjmap = [[hasjh(sequence, 0, 1)],
 for _ in range(10):
     biggest = 1
     for variationLength in range(1, math.floor(len(sequence)/2)+1):
-        same = True
-
-        varToCheck = sequence[len(sequence)-variationLength:]
         checkHash = hasjh(sequence, len(sequence)-1, variationLength)
-
-        maxNumberOfRepeats = math.floor(len(sequence)/len(varToCheck))
+        maxNumberOfRepeats = math.floor(len(sequence)/variationLength)
 
         i = 1
         repeats = 1
         while i < maxNumberOfRepeats:
-            endIndex = len(sequence) - i * len(varToCheck) - 1
+            endIndex = len(sequence) - i * variationLength - 1
             i += 1
 
             if hasjmap[endIndex][variationLength-1] != checkHash:
